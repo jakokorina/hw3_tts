c626feb68c0da75c6f034dea05a6db891c8f400a
jakokorina <jakokorina@mail.ru>
2022-11-24 15:00:45 +0300

    init

---
+++ hw_tts/loss/__init__.py
@@ -0,0 +1,5 @@
+from hw_tts.loss.CTCLossWrapper import CTCLossWrapper as CTCLoss
+
+__all__ = [
+    "CTCLoss"
+]
---
+++ hw_tts/model/__init__.py
@@ -0,0 +1,5 @@
+from hw_tts.model.baseline_model import BaselineModel
+
+__all__ = [
+    "BaselineModel",
+]
