a0e744ff75c841653af82288a0119e46b6906d2e
jakokorina <jakokorina@mail.ru>
2022-11-26 16:13:16 +0300

    bug fixes

--- hw_tts/audio/__init__.py
+++ hw_tts/audio/__init__.py
@@ -1,4 +1,4 @@
-import hparams_audio
-import tools
-import stft
-import audio_processing
+import hw_tts.audio.hparams_audio as hparams_audio
+import hw_tts.audio.tools as tools
+import hw_tts.audio.stft as stft
+import hw_tts.audio.audio_processing as audio_processing
